b9a8e54b21be66a752408bfcfae5b72a96e2215e
Fernando Petrelli <fer.petrelli@gmail.com>
2018-09-06 15:43:30 -0400

    Cleanup for tabs/spaces

--- app/ApiConsumer/Endpoints/CryptoEndpoint.php
+++ app/ApiConsumer/Endpoints/CryptoEndpoint.php
@@ -7,9 +7,10 @@ use \Petrelli\EloquentConsumer\Endpoints\BaseEndpoint;
 class CryptoEndpoint extends BaseEndpoint
 {
 
-	// Mandatory
-	protected $baseUri = 'https://api.coinmarketcap.com/v2/';
+    // Mandatory
+    protected $baseUri = 'https://api.coinmarketcap.com/v2/';
 
+    // Class used to transform the response to the package standards
     protected $transformerClass = \App\ApiConsumer\Transformers\Crypto::class;
 
 }
